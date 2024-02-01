d9f90a84923c294a7208a3fc052a60dfdf1ab562
MasayukiYamakura <mybirthdayis619@gmail.com>
2024-02-02 01:19:33 +0900

    20180617

--- pages/3_Instagram_Auto_Follow.py
+++ pages/3_Instagram_Auto_Follow.py
@@ -22,13 +22,13 @@ import time
 class Worker(threading.Thread):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
-		# self.counter = 0
+		self.counter = 0
 		self.should_stop = threading.Event()
 		
 	def run(self):
 		while not self.should_stop.wait(0):
 			time.sleep(1)
-			# self.counter += 1
+			self.counter += 1
 			# ドライバのオプション
 			options = ChromeOptions()
 
