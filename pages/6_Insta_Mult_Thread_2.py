79559337c30d0dfca36d1c58ffb549e871d96021
MasayukiYamakura <mybirthdayis619@gmail.com>
2024-02-02 02:20:09 +0900

    20180617

--- pages/6_Insta_Mult_Thread_2.py
+++ pages/6_Insta_Mult_Thread_2.py
@@ -1,18 +1,30 @@
-import threading
-import time
-import streamlit as st
+# import threading
+# import time
+# import streamlit as st
 
-def process():
-    st.write('Process Start')
-    time.sleep(3)
-    st.write('Process End')
+# def process():
+#     st.write('Process Start')
+#     time.sleep(3)
+#     st.write('Process End')
 
+
+# press_button = st.button("自動フォロー開始")
+
+# if press_button:
+#     threadA = threading.Thread(target=process)
+#     threadA.start()
+#     time.sleep(5)
+#     threadA.join()
+#     st.write('Main End')
+
+import asyncio
+
+async def main():
+    st.write('Hello ...')
+    await asyncio.sleep(1)
+    st.write('... World!')
 
 press_button = st.button("自動フォロー開始")
 
 if press_button:
-    threadA = threading.Thread(target=process)
-    threadA.start()
-    time.sleep(5)
-    threadA.join()
-    st.write('Main End')
+    asyncio.run(main())
