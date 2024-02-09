22ceb70ad7000442a23a50886f6d623a762f6bee
MasayukiYamakura <mybirthdayis619@gmail.com>
2024-02-10 00:27:49 +0900

    20180617

--- pages/5_user_agent.py
+++ pages/5_user_agent.py
@@ -16,47 +16,47 @@ press_button = st.button("自動フォロー開始")
 
 
 if press_button:
-    # プラットフォームを設定（'Firefox'を設定）
-    PLATFORM= 'iphone'
+	# プラットフォームを設定（'Firefox'を設定）
+	PLATFORM= 'iphone'
 
-    # Webサイト「IPアドレス確認・環境情報取得ツール」にアクセス
-    URL = 'https://develop.tools/env-variable/'
+	# Webサイト「IPアドレス確認・環境情報取得ツール」にアクセス
+	URL = 'https://develop.tools/env-variable/'
 
-    # インスタンスを生成
-    ua = UserAgent(PLATFORM)
+	# インスタンスを生成
+	ua = UserAgent(PLATFORM)
 
-    # ドライバのオプション
+	# ドライバのオプション
 	options = ChromeOptions()
 
-    # option設定を追加（設定する理由はメモリの削減）
-    options.add_argument("--headless")
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+	# option設定を追加（設定する理由はメモリの削減）
+	options.add_argument("--headless")
+	options.add_argument('--disable-gpu')
+	options.add_argument('--no-sandbox')
+	options.add_argument('--disable-dev-shm-usage')
 
-    # オプションを設定
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--user-agent=' + ua.set())
+	# オプションを設定
+	options.add_argument('--ignore-certificate-errors')
+	options.add_argument('--user-agent=' + ua.set())
 
-    # webdriver_managerによりドライバーをインストール
-    # chromiumを使用したいのでchrome_type引数でchromiumを指定しておく
-    CHROMEDRIVER = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-    service = fs.Service(CHROMEDRIVER)
-    driver = webdriver.Chrome(
-                                options=options,
-                                service=service
-                                )
+	# webdriver_managerによりドライバーをインストール
+	# chromiumを使用したいのでchrome_type引数でchromiumを指定しておく
+	CHROMEDRIVER = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+	service = fs.Service(CHROMEDRIVER)
+	driver = webdriver.Chrome(
+								options=options,
+								service=service
+								)
 
-    # chromedriverを使用しブラウザを起動
-    driver.get(URL)
+	# chromedriverを使用しブラウザを起動
+	driver.get(URL)
 
-    # 設定した秒数を待機後ブラウザを終了
-    SECONDS = 5
-    sleep(SECONDS)
+	# 設定した秒数を待機後ブラウザを終了
+	SECONDS = 5
+	sleep(SECONDS)
 
-    # スクリーンショットを取得
-    driver.save_screenshot('screenshot.png')
-    # 保存した画像をstreamlitアプリ上に表示
-    st.image('screenshot.png')
+	# スクリーンショットを取得
+	driver.save_screenshot('screenshot.png')
+	# 保存した画像をstreamlitアプリ上に表示
+	st.image('screenshot.png')
 
-    driver.quit()
+	driver.quit()
