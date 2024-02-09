63dafda10004e6db8e0a35f07143443977c40f5d
MasayukiYamakura <mybirthdayis619@gmail.com>
2024-02-10 00:30:31 +0900

    20180617

--- pages/5_user_agent.py
+++ pages/5_user_agent.py
@@ -50,13 +50,21 @@ if press_button:
 	# chromedriverを使用しブラウザを起動
 	driver.get(URL)
 
+    # ウィンドウ最大化
+    driver.maximize_window()
+
+
 	# 設定した秒数を待機後ブラウザを終了
 	SECONDS = 5
 	sleep(SECONDS)
 
 	# スクリーンショットを取得
 	driver.save_screenshot('screenshot.png')
-	# 保存した画像をstreamlitアプリ上に表示
-	st.image('screenshot.png')
+
+    # 全画面スクリーンショット取得　取得イメージ②
+    save_screenshot(driver, "full_size.png", is_full_size=True)
+	
+    # 保存した画像をstreamlitアプリ上に表示
+	st.image('full_size.png')
 
 	driver.quit()
